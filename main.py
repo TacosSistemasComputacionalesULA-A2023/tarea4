24816e788f3c688ded0ccac39e262d83467078ec
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-02-06 20:17:54 -0400

    Changed gamma alongside amount of training

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ if "SDL_AUDIODRIVER" in os.environ:
 
 # RobotBattery-v0, FrozenLake-v1, FrozenLake-v2
 env = gym.make('RobotBattery-v0', render_mode="human", delay=0.5)
-agent = ValueIteration(env.observation_space.n, env.action_space.n, env.P, 0.9)
+agent = ValueIteration(env.observation_space.n, env.action_space.n, env.P, 0.1)
 
-agent.solve(100)
-agent.render()
+agent.solve(1000)
 
 observation, info = env.reset()
 terminated, truncated = False, False
@@ -22,9 +21,10 @@ terminated, truncated = False, False
 env.render()
 time.sleep(2)
 
+reward = 0
 while not (terminated or truncated):
     action = agent.get_action(observation)
-    observation, _, terminated, truncated, _ = env.step(action)
+    observation, reward, terminated, truncated, _ = env.step(action)
 
 time.sleep(2)
 env.close()
